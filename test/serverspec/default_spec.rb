c2aa69076781c1711ca214424f386b761312602f
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2016-09-05 15:18:52 +0900

    ensure permission is correct

--- test/serverspec/default_spec.rb
+++ test/serverspec/default_spec.rb
@@ -4,6 +4,7 @@ require 'serverspec'
 config  = '/etc/pf.conf'
 
 describe file(config) do
+  it { should be_mode 600 }
   it { should be_file }
   its(:content) { should match /^pass\s+#/ }
   its(:content) { should match Regexp.escape('block return in on ! lo0 proto tcp to port 6000:6010') }
